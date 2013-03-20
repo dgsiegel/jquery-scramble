f83ebfb4ade64549d70cbd526c0c1af53dc3b93f
daniel g. siegel <daniel@dgsiegel.net>
2013-03-20 02:33:17 +0100

    initial import

---
+++ jquery.scramble.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2013 daniel g. siegel <daniel@dgsiegel.net>
+ *
+ * jQuery scramble plugin
+ * A jQuery plugin using the fisher yates shuffle algorithm
+ * for scrambling a set of elements
+ * http://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+ * http://en.wikipedia.org/wiki/fisher-yates_shuffle
+ *
+ * Usage: $(selector).scramble();
+ *
+ * v1.0.0 - 20/03/2013
+ *
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+*/
+
+(function($){
+  $.fn.scramble = function() {
+    var len = this.length;
+    if (len > 0) {
+      var i;
+      var tmp;
+      while (--len) {
+        i = Math.floor(Math.random() * (len + 1));
+        tmp = this[i];
+        this[i] = this[len];
+        this[len] = tmp;
+      }
+    }
+    return this;
+  };
+})(jQuery);
